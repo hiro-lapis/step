9f7ff256808d5f0c845c151dfae7a0039fada5bb
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-02 22:16:04 +0900

    SPA構成実装

--- back/routes/web.php
+++ back/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/test', function () {
-    return view('base');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/{any?}', function () {
+    return view('spa');
+})->where('any', '.*')->name('top');
