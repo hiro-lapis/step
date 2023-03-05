989ca8a836d77032583da4435c4f7bcd59a8ba93
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-05 17:26:40 +0900

    フロントエンドルーティングのログインチェック処理実装

--- back/resources/assets/ts/routes/routes.ts
+++ back/resources/assets/ts/routes/routes.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteLocationNormalized } from 'vue-router'
 
+import { useUserStore } from '../store/globalStore'
 import HelloWorld from '../components/Templates/HelloWorld.vue'
 import GoodMorning from '../components/Templates/GoodMorning.vue'
 import Login from '../components/Templates/Login.vue'
@@ -11,10 +12,20 @@ const routes = [
     { path: '/todo', name: 'todo', component: GoodMorning }, // 未作成画面の仮リンク先
     { path: '/good-morning', name: 'good-morning', component: GoodMorning },
     { path: '/login', name: 'login', component: Login },
-    { path: '/themes', name: 'themes-list', component: ThemeListView }, // テーマ一覧
+    { path: '/themes', name: 'themes-list', component: ThemeListView, },
 ]
+
 
 export const router = createRouter({
     history: createWebHistory(), // 履歴機能ON
     routes,
+})
+// 画面遷移、ブラウザリロード時共通処理
+// 認証が必要なページを開いているときはログインページへリダイレクト
+router.beforeEach(async (to: RouteLocationNormalized, from, next) => {
+    const guestPageUrl = ['/login']
+    const loginRequired = !guestPageUrl.includes(to.path)
+    const user = useUserStore()
+    if (loginRequired && !user.isLogin) next({ name: 'login' })
+    else next()
 })
