6cd9283aac0b678b2a36ab9a5683b4f303db76f2
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-06 23:03:24 +0900

    baseview作成

--- back/resources/assets/ts/components/Templates/BaseView.vue
+++ back/resources/assets/ts/components/Templates/BaseView.vue
@@ -1,8 +1,37 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { onMounted } from 'vue'
 import Loading from '../Atoms/Loading.vue'
 import NotifyMessage from '../Atoms/NotifyMessage.vue'
 import Header from '../Header.vue'
 import Footer from '../Footer.vue'
+import { useRouter } from 'vue-router'
+import { useUserStore } from '../../store/globalStore'
+
+// utilities
+const router = useRouter()
+const userStore = useUserStore()
+
+onMounted(() => {
+    // ログイン状態チェック
+    axios.get('/api/is-login')
+        .then(response => {
+            if (response.data.is_login) {
+                userStore.setLogin(true)
+                userStore.setUser(response.data.user)
+                // TODO:if-未ログイン状態のページを表示中
+                // ログイン後のTOP画面へ
+
+                router.push({ name: 'themes-list' })
+            } else {
+                // TODO:if-ログイン状態のページを表示中
+                // state更新しログイン画面へ
+                userStore.setLogin(false)
+                userStore.initUser()
+                router.push('login')
+            }
+        })
+})
 </script>
 
 <template>
