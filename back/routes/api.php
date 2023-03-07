b4663d83f55e5c4ce8ace21f4823ae52665e174d
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-08 08:09:17 +0900

    テスト配信用メール設定

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
@@ -20,7 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // ユーザーログイン状態確認
-Route::get('is-login', fn () => response()->json(['is_login' => auth()->user() !== null, 'user' => auth()->user()]));
+Route::get('is-login', function () {
+    Mail::send('emails.sample', [], function ($message) {
+        $message->to('hoge@example.com', 'Test')->subject('test mail');
+    });
+    return response()->json(['is_login' => auth()->user() !== null, 'user' => auth()->user()]);
+});
+// Route::get('is-login', fn () => response()->json(['is_login' => auth()->user() !== null, 'user' => auth()->user()]));
 
 // 認証
 Route::middleware('guest')->group(function () {
