eb853a92c74c3c46e7f6a1ab7d7f12b6e75415ff
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-09 23:36:51 +0900

    登録リクエスト実装

--- back/resources/assets/ts/apis/authRepository.ts
+++ back/resources/assets/ts/apis/authRepository.ts
@@ -1,29 +1,33 @@
 import axios from './baseRepository'
 
 export class AuthRepository {
-    private readonly loginUrl = '/api/login'
-    private readonly isLoginUrl = '/api/is-login'
-    private readonly logoutUrl = '/api/logout'
     private readonly csrfUrl = '/sanctum/csrf-cookie'
+    private readonly registerUrl = '/api/register'
+    private readonly isLoginUrl = '/api/is-login'
+    private readonly loginUrl = '/api/login'
+    private readonly logoutUrl = '/api/logout'
     private readonly forgotPasswordUrl = '/api/forgot-password'
     private readonly resetPasswordUrl = '/api/reset-password'
 
     async getCsrf(): Promise<any> {
         return await axios.get(`${this.csrfUrl}`)
     }
+    async register(params: { name: string, email: string, password: string, password_confirmation: string}): Promise<any> {
+        return await axios.post(`${this.registerUrl}`, params)
+    }
     async isLogin(): Promise<any> {
         return await axios.get(`${this.isLoginUrl}`)
     }
-    async login(data: {email: string, password: string}): Promise<any> {
-        return await axios.post(`${this.loginUrl}`, data)
+    async login(params: {email: string, password: string}): Promise<any> {
+        return await axios.post(`${this.loginUrl}`, params)
     }
     async logout(): Promise<any> {
         return await axios.post(this.logoutUrl)
     }
-    async forgotPassword(data: { email: string }): Promise<any> {
-        return await axios.post(this.forgotPasswordUrl, data)
+    async forgotPassword(params: { email: string }): Promise<any> {
+        return await axios.post(this.forgotPasswordUrl, params)
     }
-    async forgotReset(data: { email: string, token: string, password: string, password_confirmation: string }): Promise<any> {
-        return await axios.post(this.resetPasswordUrl, data)
+    async forgotReset(params: { email: string, token: string, password: string, password_confirmation: string }): Promise<any> {
+        return await axios.post(this.resetPasswordUrl, params)
     }
 }
