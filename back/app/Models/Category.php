e5c858b1182fb1dad520caaf52dffa3c05eb8439
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-10 08:11:14 +0900

    stepテーブルとmodel作成

--- back/app/Models/Category.php
+++ back/app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
@@ -12,4 +13,15 @@ class Category extends Model
 
     // マスタデータなのでアプリケーション側からの更新なし
     protected $fillable = [];
+
+    /** accessor */
+    /** relation */
+
+    /**
+     * @return BelongsTo
+     */
+    public function steps(): BelongsTo
+    {
+        return $this->belongsTo(Step::class);
+    }
 }
---
+++ back/database/migrations/2023_03_10_001420_create_steps_table.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steps', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->comment('登録ユーザーID');
+            $table->unsignedBigInteger('category_id')->comment('カテゴリーID');
+            $table->string('name')->comment('ステップ名');
+            $table->unsignedSmallInteger('achievement_date_count')->comment('達成目安時間(day)');
+            $table->time('achievement_date_time')->comment('達成目安時間(h)');
+            $table->timestamps();
+
+            $table->index('category_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steps');
+    }
+};
