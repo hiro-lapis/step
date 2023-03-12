4cd81010587e47466d4ddfbc439c6d30a5d80d0c
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-13 00:01:44 +0900

    サンプルvueファイルを使いやすくメンテ

--- back/resources/assets/ts/components/Templates/HelloWorld.vue
+++ back/resources/assets/ts/components/Templates/HelloWorld.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { inject, onMounted } from 'vue'
+import Loading from '../Atoms/Loading.vue'
+import NotifyMessage from '../Atoms/NotifyMessage.vue'
+import Header from '../Header.vue'
+import Footer from '../Footer.vue'
+import { useRouter } from 'vue-router'
 import { useUserStore } from '../../store/globalStore'
+import { Repositories } from '../../apis/repositoryFactory'
+import { guestPageName } from '../../routes/routes'
 
-console.log('type script and pinia is now!')
-// store
-const store = useUserStore()
-console.log(store)
+// utilities
+const router = useRouter()
+const userStore = useUserStore()
+const $repositories = inject<Repositories>("$repositories")!
+console.log(userStore)
 // props
 // data
 // emits
@@ -14,11 +23,15 @@ console.log(store)
 </script>
 
 <template>
-    <h1>Hello World !! and Type Script!!!</h1>
-    <div>
-        <router-link to="/good-morning">GoodMorning</router-link>
-    </div>
-    <div>
-        <router-link to="/login">Login</router-link>
-    </div>
+    <BaseView>
+        <template v-slot:content>
+            <h1>Hello World !! and Type Script!!!</h1>
+            <div>
+                <router-link to="/good-morning">GoodMorning</router-link>
+            </div>
+            <div>
+                <router-link to="/login">Login</router-link>
+            </div>
+        </template>
+    </BaseView>
 </template>
