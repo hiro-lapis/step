1752f9c6fdfb1b13ec74bb71e1bb6ce1089fa032
hiro-lapis <coolish555ddddd@gmail.com>
2023-03-13 00:01:25 +0900

    フロントのルートとリクエスト処理

--- back/resources/assets/ts/routes/routes.ts
+++ back/resources/assets/ts/routes/routes.ts
@@ -9,6 +9,7 @@ import PasswordForgot from '../components/Templates/Password/Forgot.vue'
 import PasswordReset from '../components/Templates/Password/Reset.vue'
 import ThemeListView from '../components/Templates/Themes/ListView.vue'
 import StepEditView from '../components/Templates/Steps/StepEditView.vue'
+import StepShowView from '../components/Templates/Steps/StepShowView.vue'
 import StepListView from '../components/Templates/Steps/StepListView.vue'
 
 // ルート登録
@@ -23,6 +24,7 @@ const routes = [
     { path: '/password/reset', name: 'password-reset', component: PasswordReset, },
     { path: '/steps/create', name: 'steps-create', component: StepEditView, },
     { path: '/steps', name: 'steps-list', component: StepListView, },
+    { path: '/steps/:id', name: 'steps-show', component: StepShowView, },
 ]
 
 
@@ -30,12 +32,30 @@ export const router = createRouter({
     history: createWebHistory(), // 履歴機能ON
     routes,
 })
-export const guestPageUrl = ['/register', '/login', '/password/forgot', '/password/reset']
+
+// 未ログイン状態で表示できるページ
+export const guestPageName = [
+    'register',
+    'login',
+    'password-forgot',
+    'password-reset',
+    'steps-list',
+    'steps-show'
+]
+
+// 未ログイン状態の時のみ表示するぺーこ
+export const guestOnlyPageName = [
+    'register',
+    'login',
+    'password-forgot',
+    'password-reset',
+]
 
 // 画面遷移、ブラウザリロード時共通処理
 // 認証が必要なページを開いているときはログインページへリダイレクト
 router.beforeEach(async (to: RouteLocationNormalized, from, next) => {
-    const loginRequired = !guestPageUrl.includes(to.path)
+    // ルート定義時はnameを必ず指定すること
+    const loginRequired = !guestPageName.includes(to.name!.toString())
     const user = useUserStore()
     if (loginRequired && !user.isLogin) next({ name: 'login' })
     else next()
