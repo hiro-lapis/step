0458b3504a34c4884dd7d500d7267fb4569de5be
hiro-lapis <coolish555ddddd@gmail.com>
2023-04-22 10:21:10 +0900

    不要言語ファイル削除

--- back/lang/ja/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'subject' => [
-        'invoice_paymentdetail' => '購入手続きのお知らせ',
-        'invoice_completed' => '決済完了のお知らせ',
-        'idea_is_purchased' => 'アイデア購入のお知らせ',
-    ],
-];
