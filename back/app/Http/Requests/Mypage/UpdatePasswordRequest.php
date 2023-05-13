951ce5dfbf04116004514ec9341d5e0224601d47
hiro-lapis <coolish555ddddd@gmail.com>
2023-05-14 02:54:30 +0900

    feature: #140 :ambulance: パスワードリセットモーダルを修正

---
+++ back/app/Http/Requests/Mypage/UpdatePasswordRequest.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests\Mypage;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class UpdatePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'current_password' => ['required', 'current_password:web'],
+            // 文字・数字をそれぞれ1文字以上含む8文字以上のパスワード
+            'password' => ['bail','required', 'confirmed', 'different:current_password', 'between:8,24', Password::min(8)->letters()],
+            'password_confirmation' => [],
+        ];
+    }
+}
