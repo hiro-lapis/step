c70d176a2cc87aa067e5f83cc437558b93089773
hiro-lapis <coolish555ddddd@gmail.com>
2023-05-13 20:28:54 +0900

    feature #173 :tada: パスワードリマインダでのパスワードリセット処理改修

--- back/app/Actions/Fortify/ResetUserPassword.php
+++ back/app/Actions/Fortify/ResetUserPassword.php
@@ -5,7 +5,9 @@ namespace App\Actions\Fortify;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
+use Symfony\Component\HttpFoundation\Response;
 
 class ResetUserPassword implements ResetsUserPasswords
 {
@@ -18,8 +20,17 @@ class ResetUserPassword implements ResetsUserPasswords
      */
     public function reset(User $user, array $input): void
     {
+        $password = $input['password'];
+        // 現在のパスワードと同じかチェック
+        if (Hash::check($password, $user->password)) {
+            abort(Response::HTTP_UNPROCESSABLE_ENTITY, __('validation.custom.password.different'));
+        }
+
         Validator::make($input, [
-            'password' => $this->passwordRules(),
+            // original
+            // 'password' => $this->passwordRules(), // ['required', 'string', new Password, 'confirmed']
+            // パスワード更新(MypageController@updatePassword())と同じルール
+            'password' => ['bail','required', 'confirmed', 'between:8,24', Password::min(8)->letters()],
         ])->validate();
 
         $user->forceFill([
